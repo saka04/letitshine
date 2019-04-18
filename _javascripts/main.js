20813d69bfecac33dea3a925c167c93f1db8790e
Ludovic Turmel <ludovic.turmel@liip.ch>
2019-04-18 14:18:17 +0200

    Mobile nav

--- _javascripts/main.js
+++ _javascripts/main.js
@@ -1,6 +1,7 @@
+// Components
 import Collapse from './components/Collapse';
 
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('toto');
-  new Collapse(document.querySelector('#header__mobile-links'));
-});
+// Export as Toolkit library
+export {
+  Collapse,
+};
