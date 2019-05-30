8954d8b668e52ea8699c0d97f076877cbb37a2b0
Saka04 <sacha.toufani@gmail.com>
2019-05-30 16:25:52 +0200

    Content + images prov

--- _javascripts/main.js
+++ _javascripts/main.js
@@ -3,6 +3,7 @@ import Collapse from './components/Collapse';
 import Slider from './components/Slider.js';
 //import Services from './components/Services.js';
 import SmoothScroll from './components/SmoothScroll.js'
+import HeaderScroll from './components/HeaderScroll.js'
 
 document.addEventListener('DOMContentLoaded', function() {
   if (document.querySelector('body').classList.contains('home')) {
