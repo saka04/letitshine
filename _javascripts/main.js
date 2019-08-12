9d754472a0f3e2304811ca4be67e24a81317d314
Saka04 <sacha.toufani@gmail.com>
2019-08-12 23:17:54 +0200

    gallery new + lightbox, packages update, js clean

--- _javascripts/main.js
+++ _javascripts/main.js
@@ -1,20 +1,11 @@
 // Components
 import Collapse from './components/Collapse';
-import Slider from './components/Slider.js';
-//import SmoothScroll from './components/SmoothScroll.js'
 import Nav from './components/Nav';
-
-document.addEventListener('DOMContentLoaded', function() {
-  if (document.querySelector('body').classList.contains('organiser') || 
-    document.querySelector('body').classList.contains('decorer') ||
-    document.querySelector('body').classList.contains('organize') ||
-    document.querySelector('body').classList.contains('decorate')) {
-    new Slider(document.querySelectorAll('.swiper-container'));
-  }
-});
+import Lightbox from './components/Lightbox.js'
 
 // Export as Toolkit library
 export {
   Collapse,
   Nav,
+  Lightbox,
 };
